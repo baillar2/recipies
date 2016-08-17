91ad5b8f9b9b2adfd7c810b40052a0408b4db328
Sean Baillargeon <seanpbaillargeon@gmail.com>
2016-08-16 22:06:32 -0600

    ingredient submission angular

--- homework/public/main.js
+++ homework/public/main.js
@@ -4,5 +4,27 @@ angular.module('app')
 	.controller('controllerOne', ['$scope', '$http', function($scope, $http){
 
 		var s = $scope
+		$http.get('/api/getItems')
+			.then(function(serverData){
+				console.log(serverData.data)
+				s.itemArray = serverData.data
+				console.log('ingredient list', s.itemArray)
+			})
+
+		s.item = {}
+
+		s.subIng = function(){
+			console.log('ingredient submission fired', s.item)
+			$http.post('/api/newItem', s.item)
+				.then(function(serverData){
+					console.log('returned ingredient', serverData.data)
+					alert('Your ingredient has been saved!!')
+					$http.get('/api/getItems')
+						.then(function(serverData){
+							s.itemArray = serverData.data
+							s.item = {}
+						})
+				})
+		}
 
 	}])
